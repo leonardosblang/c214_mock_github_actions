660733505caf9d98d31e34ecd24f34436a4426d1
Leonardo S.B. Lang <leolang99@gmail.com>
2022-08-29 19:10:30 -0300

    updated image and assertion method

--- test/test_professor.py
+++ test/test_professor.py
@@ -23,20 +23,20 @@ class testeProfessor(softest.TestCase):
 
     def test_Atendimento_fail(self):
         mock_professor.change_horario_de_atendimento.return_value = "Segunda 17:30"
-        self.soft_assert(self.assertEqual, mock_professor.change_horario_de_atendimento(), "Segunda 17:00",
-                         "Horario errado")
+        self.soft_assert(self.assertNotEqual, mock_professor.change_horario_de_atendimento(), "Segunda 17:00",
+                         "Horario diferente")
 
         self.assert_all()
 
     def test_Periodo_fail(self):
         mock_professor.change_periodo.return_value = "Noturno"
-        self.soft_assert(self.assertEqual, mock_professor.change_periodo(), "Integral", "Periodo errado")
+        self.soft_assert(self.assertNotEqual, mock_professor.change_periodo(), "Integral", "Periodo diferente")
 
         self.assert_all()
 
     def test_Salario_fail(self):
         mock_professor.salario.return_value = 100
-        self.soft_assert(self.assertEqual, mock_professor.salario(), 200, "Salario errado")
+        self.soft_assert(self.assertNotEqual, mock_professor.salario(), 200, "Salario diferente")
 
         self.assert_all()
 
