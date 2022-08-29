44554c8b1b4a82a7333e59941981f5cce9889053
Leonardo S.B. Lang <leolang99@gmail.com>
2022-08-29 04:28:46 -0300

    Initial commit

---
+++ professor.py
@@ -0,0 +1,25 @@
+import json
+
+class Professor:
+    def __init__(self, nome_do_professor, periodo, horario_de_atendimento):
+        self.nome_do_professor = nome_do_professor
+        self.periodo = periodo
+        self.horario_de_atendimento = horario_de_atendimento
+
+
+    def recebe_json(self, json_file):
+        with open(json_file) as f:
+            data = json.load(f)
+            self.periodo = data['periodo']
+            self.horario_de_atendimento = data['horario_De_Atendimento']
+            self.nome_do_professor = data['nome_Do_Professor']
+        return data
+
+    def change_horario_de_atendimento(self, new_hours):
+        self.horario_de_atendimento = new_hours
+
+    def change_periodo(self, new_periodo):
+        self.periodo = new_periodo
+
+    def salario(self):
+        return self.horario_de_atendimento * 100
---
+++ test/test_professor.py
@@ -0,0 +1,45 @@
+import mock
+import softest
+
+from professor import Professor
+
+# objeto mock criado usando como referencia a classe Professor
+mock_professor = mock.Mock(spec=Professor)
+
+
+# testes unitarios usando o objeto mock
+class testeProfessor(softest.TestCase):
+    def test_Atendimento(self):
+        mock_professor.change_horario_de_atendimento.return_value = "Segunda 17:30"
+        self.assertEqual(mock_professor.change_horario_de_atendimento(), "Segunda 17:30")
+
+    def test_Periodo(self):
+        mock_professor.change_periodo.return_value = "Noturno"
+        self.assertEqual(mock_professor.change_periodo(), "Noturno")
+
+    def test_Salario(self):
+        mock_professor.salario.return_value = 100
+        self.assertEqual(mock_professor.salario(), 100)
+
+    def test_Atendimento_fail(self):
+        mock_professor.change_horario_de_atendimento.return_value = "Segunda 17:30"
+        self.soft_assert(self.assertEqual, mock_professor.change_horario_de_atendimento(), "Segunda 17:00",
+                         "Horario errado")
+
+        self.assert_all()
+
+    def test_Periodo_fail(self):
+        mock_professor.change_periodo.return_value = "Noturno"
+        self.soft_assert(self.assertEqual, mock_professor.change_periodo(), "Integral", "Periodo errado")
+
+        self.assert_all()
+
+    def test_Salario_fail(self):
+        mock_professor.salario.return_value = 100
+        self.soft_assert(self.assertEqual, mock_professor.salario(), 200, "Salario errado")
+
+        self.assert_all()
+
+
+if __name__ == '__main__':
+    softest.main()
