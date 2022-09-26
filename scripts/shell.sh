3346d06cc25ebd7cabf17f14434f6e2926068665
Leonardo S.B. Lang <49520669+leonardosblang@users.noreply.github.com>
2022-09-26 00:50:17 -0300

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,5 +1,4 @@
 ls
 echo "Installing email"
 sudo apt-get install mailutils
-echo "End of installating"
 echo "Sending email using linux default email" | mail -s "subject: S107 - Leonardo Lang" leonardo.sabra@ges.inatel.br
