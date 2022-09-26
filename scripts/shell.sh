1ac4f0e8da85e7202e2ac48424a7924d77ade453
Leonardo S.B. Lang <49520669+leonardosblang@users.noreply.github.com>
2022-09-26 01:25:34 -0300

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,2 +1,2 @@
 ls
-echo "Sending E-Mail with linux default email" | mail -s "subject: S107 - Leonardo Lang" leonardo.sabra@ges.inatel.br
+echo "Sending E-Mail with linux default email" | mail -s "subject: S107 - Leonardo Lang" $EMAIL
