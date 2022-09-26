2a4746ee75587d47be7ac9a25aab0fda611cb078
Leonardo S.B. Lang <49520669+leonardosblang@users.noreply.github.com>
2022-09-26 02:23:42 -0300

    Update shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,4 +1,4 @@
 ls
 echo "aaaa"
 echo "$EMAIL"
-echo "Sending E-Mail with linux default email" | mail -s "subject: S107 - Leonardo Lang as Enviroment  Variable" EMAIL
+echo "Sending E-Mail with linux default email" | mail -s "subject: S107 - Leonardo Lang as  Enviroment  Variable" EMAIL
